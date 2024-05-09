12cea0304917782a80ceff79e93850e00541375f
Kris Lwin <65151308+HninLwin-byte@users.noreply.github.com>
2024-05-10 03:29:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,68 +9,66 @@ from llama_index.core import (
     Document, 
     SimpleDirectoryReader
 )
-
+from streamlit_extras.app_logo import add_logo
 import google.generativeai as genai
-import dotenv
-import os
 
-# Load environment variables
+import dotenv,os
 dotenv.load_dotenv()
 
-# Set page configuration
-st.set_page_config(
-    page_title="KMUTT & SCG Chatbot",
-    page_icon="🦙",
-    layout="centered",
-    initial_sidebar_state="auto",
-    menu_items=None
-)
 
-# Title and header
+st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("KMUTT & SCG Chatbot")
+add_logo("https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FSiam_Cement_Group&psig=AOvVaw3IvjVgdid7kk7EtQjmWKW9&ust=1715372952966000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPCWo660gYYDFQAAAAAdAAAAABAE")
 
-# Configure Google API
-GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+# add sidebar buttons
+st.sidebar.button("Button")
+st.sidebar.button("Button 2")
+
+# add sidebar filters
+st.sidebar.slider("Slider", 0, 100, 50)
+st.sidebar.date_input("Date Input")
+# openai.api_key = st.secrets.openai_key
+GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
-# Initialize chat messages history
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "Ask me a question about Concrete technology"}]
 
-# Load data
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about Concrete technology"}
+    ]
+
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        embed_model = GeminiEmbedding(model_name="models/embedding-001", title="this is a document")
+        # llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert o$
+        # index = VectorStoreIndex.from_documents(docs)
+        # service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        embed_model = GeminiEmbedding(
+            model_name="models/embedding-001", title="this is a document"
+            )
         service_context = ServiceContext.from_defaults(llm = Gemini(model="models/gemini-pro"), embed_model=embed_model,)
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
 index = load_data()
 
-# Initialize the chat engine
-if "chat_engine" not in st.session_state.keys():
-    st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-# User input
-if prompt := st.text_input("Your question", key="user_input"):
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-# Display chat messages
-for message in st.session_state.messages:
-    with st.container():
-        if message["role"] == "user":
-            st.text_area("User", value=message["content"], disabled=True, key="user_message")
-        else:
-            st.text_area("Assistant", value=message["content"], disabled=True, key="assistant_message")
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-# Generate response
-if st.button("Send"):
-    if st.session_state.messages[-1]["role"] != "assistant":
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = st.session_state.chat_engine.chat(prompt)
             st.write(response.response)
             message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message)
+            st.session_state.messages.append(message) # Add response to message history
