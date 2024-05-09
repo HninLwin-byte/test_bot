86f00f7bb7db019d814a550f9c500ffffd83c389
Kris Lwin <65151308+HninLwin-byte@users.noreply.github.com>
2024-05-10 03:36:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ dotenv.load_dotenv()
 
 st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("KMUTT & SCG Chatbot")
-add_logo("scg_log.jpg")
+add_logo("scg_logo.jpg")
 # add sidebar buttons
 st.sidebar.button("Button")
 st.sidebar.button("Button 2")
